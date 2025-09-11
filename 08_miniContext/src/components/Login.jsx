f3fd61d94d2d9cda1408ce7271e713215146e694
princee01 <upadhyayprince946@gmail.com>
2025-09-11 18:53:33 +0530

    context api

---
+++ 08_miniContext/src/components/Login.jsx
@@ -0,0 +1,33 @@
+import React, {useState, useContext } from 'react'
+import UserContext from '../context/userContext'
+
+//using this component we are sending data;
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const { setUser } = useContext(UserContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setUser({ username, password });
+    }
+  return (
+      <div>
+          <h2>login</h2>
+          <input type="text"
+              value={username}
+              
+              onChange={(e)=>setUsername(e.target.value)}
+              placeholder='usename' />
+          {" "}
+          <input type="text"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='password' />
+          <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+}
+
+export default Login
---
+++ 08_miniContext/src/context/UserContext.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const UserContext = React.createContext()// this a method storing into a var
+
+export default UserContext;
